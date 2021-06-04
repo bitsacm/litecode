7b752c710e965d8149c665b349796061030424f6
Aayush Paurana <f20200803@pilani.bits-pilani.ac.in>
2021-06-04 21:15:29 +0530

    final: oauth added

---
+++ server/src/oauth2/googleAuthRouters.js
@@ -0,0 +1,61 @@
+const express = require('express')
+const router = new express.Router()
+const { googleLoginUrl, getAccessTokenFromCode, getUserData } = require('./googleAuthHelpers')
+const User = require('../models/user')
+const auth = require('../middleware/auth')
+
+router.get('/auth/google', async (req, res) => {
+  await res.redirect(googleLoginUrl)
+})
+
+router.get('/auth/google/redirect', async (req, res) => {
+  const code = req.query.code
+
+  const { access_token } = await getAccessTokenFromCode(code)
+
+  const data = await getUserData(access_token)
+
+  const user = await User.findOne({ email: data.email })
+
+  // if user does not exist, create a new one or login an existing user
+  if (!user) {
+    const user = new User({
+      email: data.email,
+      name: data.name,
+      avatar: data.picture
+    })
+
+    try {
+      await user.save()
+      const token = await user.generateAuthToken()
+
+      res.cookie('jwt', token)
+      res.redirect(req.query.state || '/')
+    } catch (error) {
+      res.status(400).send(error)
+    }
+  } else {
+    try {
+      await user.save()
+      const token = await user.generateAuthToken()
+      res.cookie('jwt', token)
+      res.redirect(req.query.state || '/')
+    } catch (error) {
+      res.status(400).send(error)
+    }
+  }
+})
+
+router.post('/auth/logout', auth, async (req, res) => {
+  try {
+    req.user.tokens = req.user.tokens.filter((token) => {
+      return token.token !== req.token
+    })
+    await req.user.save()
+    res.send()
+  } catch (error) {
+    res.status(500).send()
+  }
+})
+
+module.exports = router
