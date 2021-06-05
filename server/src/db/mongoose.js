36f256d138e5476b6b6c6ec77b1191069bf421a9
cry0genic <cry0genic1103@gmail.com>
2021-06-05 17:08:45 +0530

    dockerize client

--- server/src/db/mongoose.js
+++ server/src/db/mongoose.js
@@ -5,4 +5,9 @@ mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
+}).then(()=>{
+    console.log('Database connected')
+}).catch((e)=>{
+    console.log('Error')
+    console.log(e)
 })
