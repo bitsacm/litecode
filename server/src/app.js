a48f5a8f9875e29b1272c32b1010f32126da12f0
Aayush Paurana <f20200803@pilani.bits-pilani.ac.in>
2021-06-08 17:05:57 +0530

    serve static files

--- server/src/app.js
+++ server/src/app.js
@@ -9,6 +9,8 @@ const path = require('path')
 
 const app = express()
 
+app.use(express.static(path.join(__dirname, '../../client/build')));
+
 app.use(express.json())
 app.use(cors())
 app.use(userRouter)
